90d71a1a8b442b7400274214cd85c02c7d6b6a90
Daniel Castro <castrodd@gmail.com>
2017-09-18 21:58:42 -0400

    Basic UI

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Daniel Castro on 9/18/17.
+//  Copyright © 2017 Daniel Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
